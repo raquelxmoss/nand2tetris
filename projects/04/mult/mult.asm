70e6e1e8378f78b640d7320a9f7f1de8f5a24663
Raquel Moss <raquelxmoss@gmail.com>
2020-09-13 13:47:48 +1200

    Yeah so assembly eh

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2 // Result
+M=0 // Set result to 0
+
+@R0
+D=M
+@STEP
+D;JGT
+
+// If it didn't jump, go to END -- that was a guard clause
+@END
+0;JMP
+
+// Adds R1 to R2 and removes 1 from R0.
+// If R0 is more than 0 we step again.
+(STEP)
+    // Get result
+    @R2
+    D=M
+
+    // Add R1 to it
+    @R1
+    D=D+M
+
+    // And write the result
+    @R2
+    M=D
+
+    // Reduce R0 by 1.
+    @R0
+    D=M-1
+    M=D
+
+    // If R0 is still > 0 then loop.
+    @STEP
+    D;JGT
+
+(END)
+    @END
+    0;JMP
